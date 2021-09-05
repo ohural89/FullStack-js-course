e84cb60e3cb145ece80f66eec56f32448bc00ebc
Olena Hural <o.hural89@gmail.com>
2021-09-05 17:16:15 +0300

    promt???

--- js/script-hw.js
+++ js/script-hw.js
@@ -1,5 +1,5 @@
-const message = "Hi! My name is Olena. Welcome to IT world.";
-alert(message);
+// const message = "Hi! My name is Olena. Welcome to IT world.";
+// alert(message);
 
 console.group('User Details');
     console.log('name: Olena Hural');
@@ -12,3 +12,7 @@ console.group('User Details');
     console.log('Country: UA');
     console.groupEnd();
 console.groupEnd();
+
+const value = prompt("Please enter a number!");
+console.log(typeof value); // "string"
+console.log(value); // "5"
