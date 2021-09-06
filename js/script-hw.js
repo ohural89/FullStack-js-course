863dc906f145cf8997b5cf0f0eedbde9d6a6552f
Olena Hural <o.hural89@gmail.com>
2021-09-06 15:54:15 +0300

    hw1 - training bot

--- js/script-hw.js
+++ js/script-hw.js
@@ -13,6 +13,42 @@ console.group('User Details');
     console.groupEnd();
 console.groupEnd();
 
-const value = prompt("Please enter a number!");
-console.log(typeof value); // "string"
-console.log(value); // "5"
+// const value = prompt("Please enter a number!");
+// console.log(typeof value); // "string"
+// console.log(value); // "5"
+
+// Завдання 4
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// Change code below this line
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice)
+
+// Завдання 5
+// const productName = "Droid";
+// const pricePerItem = 3500;
+
+// // Change code below this line
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message)
+
+// Завдання 18
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//     let message;
+//     // Change code below this line
+//   const totalPrice = (pricePerDroid * orderedQuantity)
+//     // Change code above this line
+//         if (totalPrice > customerCredits) {
+//             message = 'Insufficient funds!';
+//         } else {
+//             message = 'You ordered ${orderedQuantity} droids, you have ${customerCredits} credits left';
+// }
+//     return message;
+// }
+
+//     makeTransaction(3000, 5, 23000);
+//     makeTransaction(1000, 3, 15000);
+//     makeTransaction(5000, 10, 8000);
+//     makeTransaction(2000, 8, 10000);
+//     makeTransaction(500, 10, 5000);
