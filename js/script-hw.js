1acc726706946b47540893efcc82c0461d4ffdda
Olena Hural <o.hural89@gmail.com>
2021-09-19 12:31:45 +0300

    mentor19/09

--- js/script-hw.js
+++ js/script-hw.js
@@ -148,3 +148,53 @@ console.groupEnd();
 //     // Change code above this line
 //     return message;
 // }
+
+// //  Задача бот Модуль 2
+// function splitMessage(message, delimeter) {
+//     let words;
+
+//     const message = delimeter.split(" ");
+//     const message = delimeter.split("");
+//     const message = delimeter.split("_");
+//         console.log(phrase);
+//         console.log(word);
+//         console.log(partPhrase);
+
+//     return words;
+// }
+
+// splitMessage("Mango hurries to the train", " ");
+// splitMessage("Mango", "");
+// splitMessage("best_for_week", "_");
+
+// Практика з ментором 19.09.2021
+
+// Практичні задачі
+
+// Задача 1
+//  Дан массив с числами. Создайте из него новый массив, где останутся лежать только положительные числа. Создайте для этого вспомогательную функцию isPositive(), которая параметром будет принимать число и возвращать true, если число положительное, и false - если отрицательное.
+const arr = [1, 2, 3, -1, -2, -3];
+
+// Задача 2
+// Палиндром — слово, предложение или последовательность символов, которая абсолютно одинаково читается как в привычном направлении, так и в обратном. К примеру, “Anna” — это палиндром, а “table” и “John” — нет.
+// Дана строка; нужно написать функцию, которая позволяет вернуть значение true, если строка является палиндромом, и false — если нет. При этом нужно учитывать пробелы и знаки препинания.
+function polindrom(srting) {
+    const lowerCaseString = string.toLowerCase();
+
+    return lowerCaseString === lowerCaseString.split("").reverse().join("");
+}
+console.log(polindrom("Anna"));
+
+// Задача 3
+
+
+
+
+
+
+
+
+
+
+
+
