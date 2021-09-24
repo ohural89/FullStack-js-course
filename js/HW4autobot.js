d850c6564e4a8208c6661be618947ef8209a99a7
Olena Hural <o.hural89@gmail.com>
2021-09-24 23:25:34 +0300

    hw4 - tasks 45 done

--- js/HW4autobot.js
+++ js/HW4autobot.js
@@ -714,11 +714,37 @@
 // console.log(sortedByDescentingRating);
 
 // Task 45 (chaining) метод ланцюжків
-const MIN_BOOK_RATING = 8;
-// Change code below this line
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood",
+//     author: "Tanith Lee", 
+//     rating: 7.94 },
+//   {
+//     title: "The Dreams in the Witch House",
+//     author: "Howard Lovecraft",
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+// // Change code below this line
+// const names = books.filter(book => book.rating > MIN_BOOK_RATING)
+// .map(book => book.author).sort((firstAuthor, secondAuthor) => firstAuthor.localeCompare(secondAuthor));
 
-const names = books;
-
+// console.log(names);
 
 // Task 30 every(), 32 some()
 // const firstArray = [26, 94, 36, 18];
